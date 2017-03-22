bfd7ce6b040d991855ed4fe69be23242c6d9592c
AdrianMladenov <mladenov.adrian@gmail.com>
2017-03-22 16:27:07 +0200

    Initialize
    Project.Start();

---
+++ WoW/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WoW.Startup))]
+namespace WoW
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

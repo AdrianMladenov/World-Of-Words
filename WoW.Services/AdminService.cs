8aa682a6fa383ac6b6f55cab23b934f7c9bcc90e
Alexander-Lazarov88 <alexander_lazarov88@abv.bg>
2017-04-17 13:57:49 +0300

    Start admin panel

---
+++ WoW.Services/AdminService.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WoW.Models.EntityModels;
+using WoW.Models.ViewModels.Admin;
+using WoW.Models.ViewModels.Words;
+
+namespace WoW.Services
+{
+  public class AdminService : Service
+    {
+        public APIndexVM ShowAdminPage()
+        {
+            APIndexVM page = new APIndexVM();
+
+            IEnumerable<WordForValidate> wordsForValidate = Context.WordsForValidation;
+            IEnumerable<Word> words = Context.Words;
+            IEnumerable<ApplicationUser> users = Context.Users;
+
+            IEnumerable<AllWordsOfUser> allwords = Mapper.Map<IEnumerable<WordForValidate>, IEnumerable<AllWordsOfUser>>(wordsForValidate);
+
+            IEnumerable<APUsersInfoVM> usersInfo = Mapper.Map<IEnumerable<ApplicationUser>, IEnumerable<APUsersInfoVM>>(users);
+
+            page.AllWordsForValidate = allwords;
+            page.UsersInfo = usersInfo;
+
+            return page;
+        }
+    }
+}

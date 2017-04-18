da24690e99cf195b1f3053243a14cea9755435da
Alexander-Lazarov88 <alexander_lazarov88@abv.bg>
2017-04-18 13:30:53 +0300

    Working on Enums

--- WoW.Models/ViewModels/User/Info.cs
+++ WoW.Models/ViewModels/User/Info.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WoW.Models.EntityModels;
 
 namespace WoW.Models.ViewModels.User
 {
@@ -15,13 +16,13 @@ namespace WoW.Models.ViewModels.User
         public int Age { get; set; }
 
         public string Email { get; set; }
+        
+        public EntityModels.Enum.Gender Gender { get; set; }
 
-        public string Gender { get; set; }
+        public EntityModels.Enum.EducationDegree EducationDegree { get; set; }
 
-        public string EducationDegree { get; set; }
+        public EntityModels.Enum.WorkingSphere WorkingSphere { get; set; }
 
-        public string WorkingSphere { get; set; }
-
-        public string SocialStatus { get; set; }
+        public EntityModels.Enum.SocialStatus SocialStatus { get; set; }
     }
 }
--- WoW.Models/ViewModels/User/ProfileVM.cs
+++ WoW.Models/ViewModels/User/ProfileVM.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using WoW.Models.EntityModels;
 using WoW.Models.ViewModels.Words;
 
+
 namespace WoW.Models.ViewModels.User
 {
     public class ProfileVM
@@ -15,5 +16,6 @@ namespace WoW.Models.ViewModels.User
         public IEnumerable<AllWordsOfUser> AllWords { get; set; }
 
         public Info Info { get; set; }
+        
     }
 }
